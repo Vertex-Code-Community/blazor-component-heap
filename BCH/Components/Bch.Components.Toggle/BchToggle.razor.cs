6cd17333724bcc5a9b64eeb9fd4ed85d70269bd6
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-08-19 18:55:50 +0300

    add themes to toggle component

--- BCH/Components/Bch.Components.Toggle/BchToggle.razor.cs
+++ BCH/Components/Bch.Components.Toggle/BchToggle.razor.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Bch.Modules.Themes.Models;
+using Bch.Modules.Themes.Attributes;
+using Bch.Modules.Themes.Extensions;
 
 namespace Bch.Components.Toggle;
 
@@ -25,6 +28,35 @@ public partial class BchToggle
     [Parameter] public string CheckedBackgroundColor { get; set; } = string.Empty;
     [Parameter] public string DefaultCircleColor { get; set; } = string.Empty;
     [Parameter] public string DefaultBackgroundColor { get; set; } = string.Empty;
+    [Parameter] public BchTheme? Theme { get; set; }
+    [CascadingParameter] public BchTheme? ThemeCascading { get; set; }
 
     private string _toggleId = $"_id_{Guid.NewGuid()}";
+    private BchTheme EffectiveTheme => Theme ?? ThemeCascading ?? BchTheme.LightGreen;
+    private string GetThemeCssClass() => EffectiveTheme.GetValue<string, CssNameAttribute>(a => a.CssName) ?? string.Empty;
+
+    private string ResolveDefaultBackgroundColor()
+        => string.IsNullOrWhiteSpace(DefaultBackgroundColor)
+            ? "#D7D7D7"
+            : DefaultBackgroundColor;
+
+    private string ResolveDefaultCircleColor()
+        => string.IsNullOrWhiteSpace(DefaultCircleColor)
+            ? "#FFFFFF"
+            : DefaultCircleColor;
+
+    private string ResolveCheckedCircleColor()
+        => string.IsNullOrWhiteSpace(CheckedCircleColor)
+            ? "#FFFFFF"
+            : CheckedCircleColor;
+
+    private string ResolveCheckedBackgroundColor()
+        => string.IsNullOrWhiteSpace(CheckedBackgroundColor)
+            ? EffectiveTheme switch
+            {
+                BchTheme.Dark => "#4B5563",
+                BchTheme.Light => "#6C757D",
+                _ => "#33B469"
+            }
+            : CheckedBackgroundColor;
 }
