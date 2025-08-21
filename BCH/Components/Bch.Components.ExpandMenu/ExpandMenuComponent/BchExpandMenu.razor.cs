da079238b44e7c5cfae23195cc8594a57b78e573
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-08-21 12:23:38 +0300

    add themes to expand menu component

--- BCH/Components/Bch.Components.ExpandMenu/ExpandMenuComponent/BchExpandMenu.razor.cs
+++ BCH/Components/Bch.Components.ExpandMenu/ExpandMenuComponent/BchExpandMenu.razor.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Bch.Components.ExpandMenu.ExpandMenuContainer;
 using Bch.Modules.DomInterop.Services;
+using Bch.Modules.Themes.Models;
+using Bch.Modules.Themes.Attributes;
+using Bch.Modules.Themes.Extensions;
 
 namespace Bch.Components.ExpandMenu.ExpandMenuComponent;
 
@@ -13,9 +16,14 @@ public partial class BchExpandMenu : IDisposable
     [Parameter] public string Title { get; set; } = string.Empty;
     [Parameter] public string Description { get; set; } = string.Empty;
     [Parameter] public bool Disabled { get; set; }
+    [Parameter] public BchTheme? Theme { get; set; }
+    [CascadingParameter] public BchTheme? ThemeCascading { get; set; }
 
     private readonly string _descriptionId = $"_id_{Guid.NewGuid()}";
     private double _heightDescription = 0;
+
+    private BchTheme EffectiveTheme => Theme ?? ThemeCascading ?? BchTheme.LightGreen;
+    private string GetThemeCssClass() => EffectiveTheme.GetValue<string, CssNameAttribute>(a => a.CssName) ?? string.Empty;
 
     protected override void OnInitialized()
     {
